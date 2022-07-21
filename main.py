bd415922fe203cc9b781fe3ffd8841ee1aae5a7d
NAVANJANA <107739539+EpicBotSl@users.noreply.github.com>
2022-07-21 16:22:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,43 @@
-import os
-import asyncio
-from bot import bot
-from config import Config
-from pyrogram import idle
-from helpers.log import LOGGER
-from helpers.utils import start_stream
-from assets.user import group_call, USER
-from pyrogram.errors import UserAlreadyParticipant
+from config import Config 
+from config import *
+import logging
+from pyrogram import Client, idle
+from pyromod import listen  # type: ignore
+from pyrogram.errors import ApiIdInvalid, ApiIdPublishedFlood, AccessTokenInvalid
+
+logging.basicConfig(
+    level=logging.WARNING, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+
+bot = Client(
+    "Epic Developers",
+    api_id=Config.API_ID,
+    api_hash=Config.API_HASH,
+    bot_token=Config.BOT_TOKEN,
+    in_memory=True,
+    plugins=dict(root="module"),
+)
 
 
-if not os.path.isdir("./downloads"):
-    os.makedirs("./downloads")
-else:
-    for f in os.listdir("./downloads"):
-        os.remove(f"./downloads/{f}")
-
-
-bot.start()
-
-if __name__ == '__main__':
-    asyncio.get_event_loop().run_until_complete(main())
+if __name__ == "__main__":
+    print("⌛Updating & Analyzing bot⏳")
+    try:
+        bot.start()
+    except (ApiIdInvalid, ApiIdPublishedFlood):
+        raise Exception("Your API_ID/API_HASH is not valid.")
+    except AccessTokenInvalid:
+        raise Exception("Your BOT_TOKEN is not valid.")
+    uname = bot.get_me().username
+    print(f"""@{uname} is Accepted
+╔════╗────────╔═══╗
+║╔╗╔╗║────────║╔══╝
+╚╝║║╠╩═╦══╦╗╔╗║╚══╦══╦╦══╗
+──║║║║═╣╔╗║╚╝║║╔══╣╔╗╠╣╔═╝
+──║║║║═╣╔╗║║║║║╚══╣╚╝║║╚═╗
+──╚╝╚══╩╝╚╩╩╩╝╚═══╣╔═╩╩══╝
+──────────────────║║
+──────────────────╚╝
+Join For update @EpicBotsSl""")
+    idle()
+    bot.stop()
+    print("Bot stopped. Alvida!")
