ef9164fc10cdc178dbe5ef2a214717af556cdeb0
NAVANJANA <107739539+EpicBotSl@users.noreply.github.com>
2022-07-21 16:16:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,21 +17,6 @@ else:
 
 async def main():
     await bot.start()
-    Config.BOT_USERNAME = (await bot.get_me()).username
-    await group_call.start()
-    LOGGER.warning(f"{Config.BOT_USERNAME} Started Successfully !")
-    if Config.IS_NONSTOP_STREAM:
-        await start_stream()
-    try:
-        await USER.join_chat("AsmSafone")
-    except UserAlreadyParticipant:
-        pass
-    except Exception as e:
-        print(e)
-        pass
-    await idle()
-    LOGGER.warning("Video Player Bot Stopped !")
-    await bot.stop()
 
 if __name__ == '__main__':
     asyncio.get_event_loop().run_until_complete(main())
