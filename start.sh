5316efb27800baacae75da30a680078cf491bc1c
NAVANJANA <107739539+EpicBotSl@users.noreply.github.com>
2022-07-21 15:43:17 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,7 @@
+echo "Cloning Repo, Please Wait..."
+git clone -b main https://github.com/EpicBotSl/EpicCricketVcBot.git /EpicCricketVcBot
+cd /VideoPlayerBot
+echo "Installing Requirements..."
+pip3 install -U -r requirements.txt
+echo "Starting Bot, Please Wait..."
+python3 main.py
